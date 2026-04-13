63bc401e5b253fdde9552d8fcbb3739c45b1a328
Akankshya1508 <84058991+Akankshya1508@users.noreply.github.com>
2026-04-13 15:31:33 +0530

    Time: 0 ms (100%), Space: 42.8 MB (43.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -21,20 +21,4 @@ class Solution {
         return prev1;
         
     }
-    int func(int ind, int nums[],int dp[])
-    {
-        if(ind==0)
-        return nums[ind];
-        
-        if(dp[ind]!=-1)
-        return dp[ind];
-
-        int npick=0+func(ind-1,nums,dp);
-        int pick=nums[ind];
-
-        if(ind>1)
-        pick+=func(ind-2,nums,dp);
-
-        return dp[ind]=Math.max(pick,npick);
-    }
 }
