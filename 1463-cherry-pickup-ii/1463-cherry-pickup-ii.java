4222f79ac5a35d318a9a1eecbd045943f31a806f
Akankshya1508 <84058991+Akankshya1508@users.noreply.github.com>
2026-04-26 11:11:40 +0530

    Time: 42 ms (9.52%), Space: 46.3 MB (65%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -4,57 +4,60 @@ class Solution {
         int r = grid.length;
         int c = grid[0].length;
 
-        int dp[][][] = new int[r][c][c];
+        int dp[][][]=new int[r][c][c];
 
-        for(int i = 0; i < r; i++)
-            for(int j = 0; j < c; j++)
-                Arrays.fill(dp[i][j], -1);
-
-        return solve(0, 0, c - 1, grid,dp);
-        
-    }
-      int solve(int row, int col1, int col2, int[][] grid,int dp[][][])
-    {
-        int r = grid.length;
-        int c = grid[0].length;
-
-        // ❌ Out of bounds
-        if(col1 < 0 || col2 < 0 || col1 >= c || col2 >= c)
-            return (int)-1e9;
-
-        // ✅ Last row
-        if(row == r - 1)
+         // 🟢 Base case: last row
+        for(int col1 = 0; col1 < c; col1++)
         {
-            if(col1 == col2)
-                return grid[row][col1];
-            else
-                return grid[row][col1] + grid[row][col2];
-        }
-
-        // 🔁 Memo check
-        if(dp[row][col1][col2] != -1)
-            return dp[row][col1][col2];
-
-        int max = 0;
-
-        // Try all 9 moves
-        for(int d1 = -1; d1 <= 1; d1++)
-        {
-            for(int d2 = -1; d2 <= 1; d2++)
+            for(int col2 = 0; col2 < c; col2++)
             {
-                int value;
-
                 if(col1 == col2)
-                    value = grid[row][col1];
+                    dp[r-1][col1][col2] = grid[r-1][col1];
                 else
-                    value = grid[row][col1] + grid[row][col2];
-
-                value += solve(row + 1, col1 + d1, col2 + d2, grid,dp);
-
-                max = Math.max(max, value);
+                    dp[r-1][col1][col2] = grid[r-1][col1] + grid[r-1][col2];
             }
         }
 
-        return dp[row][col1][col2] = max;
+        // 🔁 Fill from bottom to top
+        for(int row = r - 2; row >= 0; row--)
+        {
+            for(int col1 = 0; col1 < c; col1++)
+            {
+                for(int col2 = 0; col2 < c; col2++)
+                {
+                    int max = 0;
+
+                    for(int d1 = -1; d1 <= 1; d1++)
+                    {
+                        for(int d2 = -1; d2 <= 1; d2++)
+                        {
+                            int newCol1 = col1 + d1;
+                            int newCol2 = col2 + d2;
+
+                            if(newCol1 >= 0 && newCol1 < c && newCol2 >= 0 && newCol2 < c)
+                            {
+                                int value;
+
+                                if(col1 == col2)
+                                    value = grid[row][col1];
+                                else
+                                    value = grid[row][col1] + grid[row][col2];
+
+                                value += dp[row+1][newCol1][newCol2];
+
+                                max = Math.max(max, value);
+                            }
+                        }
+                    }
+
+                    dp[row][col1][col2] = max;
+                }
+            }
+        }
+
+        // 🚀 Start positions
+        return dp[0][0][c-1];
+        
     }
+     
 }
